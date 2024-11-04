8e48ac8454781c212d18be9c234c98e87a044198
GeonwooVincentKim <kdsnop@gmail.com>
2024-11-04 18:17:12 +0900

    [Update] - Main.cpp and PhoneBook.cpp

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -12,34 +12,35 @@
 
 #include "PhoneBook.hpp"
 #include <iostream>
-#include <iomanip> // For formatting output
 
 PhoneBook::PhoneBook() : contactCount(0), nextIndex(0) {}
 
-// Adds a contact to the phonebook
 void PhoneBook::addContact(const Contact &contact) {
     contacts[nextIndex] = contact;
-    nextIndex = (nextIndex + 1) % 8; // Circular buffer logic
+    nextIndex = (nextIndex + 1) % 8;
     if (contactCount < 8) {
         contactCount++;
     }
 }
 
-// Display the list of contacts
-void PhoneBook::displayContacts() const {
-    std::cout << std::setw(10) << "Index" << "|"
-              << std::setw(10) << "First Name" << "|"
-              << std::setw(10) << "Last Name" << "|"
-              << std::setw(10) << "Nickname" << std::endl;
-    for (int i = 0; i < contactCount; i++) {
-        std::cout << std::setw(10) << i + 1 << "|"
-                  << std::setw(10) << (contacts[i].getFirstName().length() > 10 ? contacts[i].getFirstName().substr(0, 9) + "." : contacts[i].getFirstName()) << "|"
-                  << std::setw(10) << (contacts[i].getLastName().length() > 10 ? contacts[i].getLastName().substr(0, 9) + "." : contacts[i].getLastName()) << "|"
-                  << std::setw(10) << (contacts[i].getNickname().length() > 10 ? contacts[i].getNickname().substr(0, 9) + "." : contacts[i].getNickname()) << std::endl;
+std::string formatField(const std::string &field) {
+    if (field.length() > 10) {
+        return field.substr(0, 9) + ".";
+    } else {
+        return std::string(10 - field.length(), ' ') + field;
     }
 }
 
-// Display details of a specific contact by index
+void PhoneBook::displayContacts() const {
+    std::cout << "     Index|First Name| Last Name|  Nickname" << std::endl;
+    for (int i = 0; i < contactCount; i++) {
+        std::cout << "         " << i + 1 << "|"
+                  << formatField(contacts[i].getFirstName()) << "|"
+                  << formatField(contacts[i].getLastName()) << "|"
+                  << formatField(contacts[i].getNickname()) << std::endl;
+    }
+}
+
 void PhoneBook::displayContactDetails(int index) const {
     if (index < 0 || index >= contactCount) {
         std::cout << "Invalid index." << std::endl;
